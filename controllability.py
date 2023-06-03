dfcbaa26821a8bd778cabe63480ecee6ce8013a0
xining-chen <106721914+xining-chen@users.noreply.github.com>
2023-06-02 18:18:41 -0600

    size vs network control

---
+++ controllability.py
@@ -0,0 +1,80 @@
+import networkx as nx
+import scipy.linalg as la
+import numpy as np
+import control
+
+def modal_control(G, stabilize=True):
+    """
+    # Modal controllability based on code from Basset
+    # https://complexsystemsupenn.com/codedata
+    :param G: numpy array connectome
+    :param stabilize: stabilize by ensuring largest e-value is 1.
+    :return: modal controllability for each node
+    """
+    A = nx.to_numpy_array(G)    # Order in G.nodes()
+    if stabilize:
+        # Matrix norm
+        normA = la.svdvals(A)[0] + 1
+        A = A / normA
+    # Schur decomp - stability
+    T, U = la.schur(A, output='real')
+    eVals = np.diag(T)
+    N = len(eVals)
+    phi = np.empty([N, ])
+    for i in range(0, N):
+        phi[i] = np.dot(U[i, :]**2, (1 - eVals**2))
+
+    nodeList = G.nodes()
+    modalCtrbDict = {}
+    for i, n in enumerate(nodeList):
+        modalCtrbDict[str(n+1)] = phi[i]
+    return modalCtrbDict
+
+
+def avg_control(G, stabilize=True):
+    """
+    Average controllability based on code from Basset
+    https://complexsystemsupenn.com/codedata
+    :param G: numpy array connectome
+    :param stabilize: stabilize by ensuring largest e-value is 1.
+    :return: average controllability for each node
+    """
+    A = nx.to_numpy_array(G)
+    if stabilize:
+        normA = la.svdvals(A)[0] + 1
+        A = A / normA
+    T, U = la.schur(A, output='real')
+    eVals = np.diag(T)
+    eVals.shape = [len(A[0,:]), 1]
+    midMat = (U**2).transpose()          # ******** is U guaranteed to be real?
+    P = np.tile(1-eVals**2, (1, len(A[0,:])))
+    res = sum(midMat/P)     # row vector
+
+    nodeList = G.nodes()
+    ac = {}
+    for i, n in enumerate(nodeList):
+        ac[str(n+1)] = res[i]
+    return ac
+
+
+def avg_control2(G):
+    """
+    Average controllability based on new_ave_control.m - from Emma Towlson.
+    https://python-control.readthedocs.io/en/latest/control.html#system-creation
+    :param G:
+    :return:
+    """
+    A = nx.to_numpy_array(G)
+    normA = la.svdvals(A)[0] + 1
+    A = A / normA
+    n = A.shape[0]
+    nodeList = G.nodes()
+    ac = {}
+    # Calculate for each node i
+    for i, node in enumerate(nodeList):
+        Bi = np.zeros((n, 1))
+        Bi[i] = 1
+        sys = control.ss(A, Bi, np.identity(n), [])     # make a discrete time system
+        Wc = control.gram(sys, 'c')                     # calculate the controllability grammian
+        ac[node] = np.trace(Wc)                            # calculate trace
+    return ac
--- data_io.py
+++ data_io.py
@@ -6,8 +6,12 @@ Author(s): Xining Chen
 import os
 import networkx as nx
 import pickle as pkl
+import importlib
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import controllability as ctrb
+importlib.reload(ctrb)
 from scipy.io import loadmat
 
 
@@ -29,10 +33,52 @@ def open_data(path, num=1064, ftype="graphml"):
             # --- Alison ---
             # Add code here to convert nx graph --> Adj matrix.
             # Store adj. matrix into a variable called avg_mat (you need to define this above, outside the loops)
-            # Take the average using 'c' (counting how many files read) by avg_mat/c and then return that value. 
+            # Take the average using 'c' (counting how many files read) by avg_mat/c and then return that value.
             # -----
             all_data[file] = G
             c += 1
+    return all_data
+
+
+def open_data2(path, num=1064, ftype="graphml"):
+    all_data = {}
+    c = 0
+    min_w = 100000
+    max_w = 0
+    mW = 0
+    for root, dirs, files in os.walk(path):
+        for file in tqdm(files):
+            if c >= num:
+                break
+            if not file.endswith(f".{ftype}"):
+                continue
+            G = nx.read_graphml(f'{path}{file}')
+            adjMat = nx.to_numpy_array(G, weight="number_of_fibers")
+            if np.count_nonzero(adjMat) == 0:
+                print("No brain data #", file)
+                continue
+            meanWeight = adjMat.sum() / np.count_nonzero(adjMat)
+            if meanWeight < min_w:
+                min_w = meanWeight
+            if meanWeight > max_w:
+                max_w = meanWeight
+            mW += meanWeight
+
+            adjMat = adjMat/meanWeight
+            G_norm = nx.from_numpy_array(adjMat)
+
+            avgCtrbDict = ctrb.avg_control(G_norm)
+            nx.set_node_attributes(G, avgCtrbDict, name='avgCtrb')
+
+            modalCtrbDict = ctrb.modal_control(G_norm)
+            nx.set_node_attributes(G, modalCtrbDict, name='modCtrb')
+
+            all_data[file] = G
+            c += 1
+    print("Group connectome edge weight info: ")
+    print("\tmin mean W:", min_w)
+    print("\tmax mean W:", max_w)
+    print("\tGroup mean W:", mW/c)
     return all_data
 
 
@@ -50,7 +96,6 @@ def save_to_pickle(data, path, pickle_name):
     __check_path(file_path)
     with open(file_path, 'wb') as f:
         pkl.dump(data, f)
-    f.close()
     print(f"Saved to {file_path}.")
 
 
